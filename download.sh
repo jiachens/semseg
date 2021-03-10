7a6c1e8902faab51f38abb6b1adeb58f7cd0669e
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-03-09 19:51:21 -0500

    update

--- download.sh
+++ download.sh
@@ -3,12 +3,12 @@
  # @Autor: Jiachen Sun
  # @Date: 2021-03-09 18:17:42
  # @LastEditors: Jiachen Sun
- # @LastEditTime: 2021-03-09 19:49:14
+ # @LastEditTime: 2021-03-09 19:51:05
 ### 
 python gdrivedl.py https://drive.google.com/file/d/1vzmR0Yf0SF_EbK6DLxrpA2hNj0paB68G/view?usp=sharing
 python gdrivedl.py https://drive.google.com/file/d/1iinRZzMqBsLr52R936Rcq7IlFl0cWd4P/view?usp=sharing
 
 mkdir -p dataset
-ln -s /cityspace/ dataset/cityscapes
+ln -s /cityspace dataset/cityscapes
 
 mv train_epoch_200.pth ./exp/cityscapes/psanet50/model/
