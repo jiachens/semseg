efe5c8b39b84d756c6c4143cccd38311e92fd86d
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-03-10 18:28:21 -0500

    test

--- tool/test.py
+++ tool/test.py
@@ -130,7 +130,7 @@ def net_process(model, image, label, mean, std=None, flip=False):
     # image = image.transpose((2, 0, 1))
     # image[:300,:300,:] = p_img
     # cv2.imwrite('./test.png',np.uint8(image))
-    patch_img = np.zeros_like(image.transpose((2, 0, 1)).unsqueeze(0))
+    patch_img = np.zeros_like(np.expand_dims(image.transpose((2, 0, 1)),0))
     p_img = p_img/255.
     p_img = np.moveaxis(p_img,-1,0)
     patch_img[:,:,:300,:300] = p_img
