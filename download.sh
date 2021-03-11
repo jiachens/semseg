9c051ebde204721e91e43f01181f41d929ac4b93
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-03-10 19:52:42 -0500

    test

--- download.sh
+++ download.sh
@@ -3,8 +3,10 @@
  # @Autor: Jiachen Sun
  # @Date: 2021-03-09 18:17:42
  # @LastEditors: Jiachen Sun
- # @LastEditTime: 2021-03-09 19:51:05
+ # @LastEditTime: 2021-03-10 19:52:38
 ### 
+
+pip install kornia
 python gdrivedl.py https://drive.google.com/file/d/1vzmR0Yf0SF_EbK6DLxrpA2hNj0paB68G/view?usp=sharing
 python gdrivedl.py https://drive.google.com/file/d/1iinRZzMqBsLr52R936Rcq7IlFl0cWd4P/view?usp=sharing
 
